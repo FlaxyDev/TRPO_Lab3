4f80a2243e73fde7fbac2b9889e606da684342ff
Diak Maksym <55993687+FlaxyDev@users.noreply.github.com>
2023-09-23 02:04:19 +0300

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    using System;
+
+    public class Matrix<T> where T : IComparable<T>, new()
+    {
+        private T[,] matrix;
+
+        public Matrix(int rows, int columns)
+        {
+            matrix = new T[rows, columns];
+        }
+
+        public T this[int row, int column]
+        {
+            get { return matrix[row, column]; }
+            set { matrix[row, column] = value; }
+        }
+
+        public int Rows
+        {
+            get { return matrix.GetLength(0); }
+        }
+
+        public int Columns
+        {
+            get { return matrix.GetLength(1); }
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        public T SumNonNegativeColumns()
+        {
+            dynamic sum = default(T);
+            for (int col = 0; col < Columns; col++)
+            {
+                bool hasNegative = false;
+                for (int row = 0; row < Rows; row++)
+                {
+                    if (matrix[row, col].CompareTo(new T()) < 0)
+                    {
+                        hasNegative = true;
+                        break;
+                    }
+                }
+                if (!hasNegative)
+                {
+                    for (int row = 0; row < Rows; row++)
+                    {
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public T MaxSumOfParallelDiagonals()
+        {
+            dynamic maxSum = default(T);
+            for (int k = 0; k < Rows; k++)
+            {
+                dynamic diagonalSum = default(T);
+                for (int i = 0, j = k; i < Rows && j < Columns; i++, j++)
+                {
+                    diagonalSum += matrix[i, j];
+                }
+                if (diagonalSum.CompareTo(maxSum) > 0)
+                {
+                    maxSum = diagonalSum;
+                }
+            }
+
+            for (int k = 1; k < Columns; k++)
+            {
+                dynamic diagonalSum = default(T);
+                for (int i = k, j = 0; i < Rows && j < Columns; i++, j++)
+                {
+                    diagonalSum += matrix[i, j];
+                }
+                if (diagonalSum.CompareTo(maxSum) > 0)
+                {
+                    maxSum = diagonalSum;
+                }
+            }
+            return maxSum;
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix<int> intMatrix = new Matrix<int>(3, 3);
+            intMatrix[0, 0] = 1;
+            intMatrix[0, 1] = 2;
+            intMatrix[0, 2] = 30;
+            intMatrix[1, 0] = 4;
+            intMatrix[1, 1] = 5;
+            intMatrix[1, 2] = 6;
+            intMatrix[2, 0] = 7;
+            intMatrix[2, 1] = 8;
+            intMatrix[2, 2] = 9;
+
+            intMatrix.Display();
+
+            Console.WriteLine($"Max sum of Diagonals: {intMatrix.MaxSumOfParallelDiagonals()}");
+            Console.WriteLine($"Sum Non Negative Columns: {intMatrix.SumNonNegativeColumns()}");
+        }
+    }
+}
